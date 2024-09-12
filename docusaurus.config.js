0e19f3df1aada024c5eaa9f11c04bc3bbd7c2980
Nguyễn Minh Chí <minhchico300kc@gmail.com>
2024-09-12 13:15:24 +0700

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
         },
         items: [
           {
-            href: "https://github.com/minhchi1509",
+            href: "https://github.com/nmc-docs/html",
             label: "GitHub",
             position: "right",
           },
